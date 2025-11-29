701ff81d58dc033031fc7859737c6e53f022f16a
Etin Horng <eltonhong000@gmail.com>
2025-11-29 16:58:06 +0800

    precise ms

--- ws6/soft_timer_v2/call_back.c
+++ ws6/soft_timer_v2/call_back.c
@@ -15,8 +15,9 @@ void cb_A(void *arg){
         // printf("[A] one-shot 100ms fired at %ld.%06ld\n",
         //     (long)tv.tv_sec- tv_start_, (long)tv.tv_usec-tv_start_u);
     
-    long ttt = ((long)tv.tv_sec-tv_start_)*1000 + ((long)tv.tv_usec-tv_start_u)/1000;
-    printf("[A] one-shot 100ms fired at  %ld ms\n", ttt);
+    long ttt = ((long)tv.tv_sec-tv_start_)*1000000 + ((long)tv.tv_usec-tv_start_u);
+    float ttt2 = (float)ttt/1000;
+    printf("[A] one-shot 100ms fired at  %f ms\n", ttt2);
     /* 故意在 callback 裡 sleep，觀察 bounded inaccuracy */
     usleep(50 * 1000);  /* 50ms */
 }
@@ -28,8 +29,9 @@ void cb_B(void *arg){
     //        (long)tv.tv_sec, (long)tv.tv_usec);
     // printf("[B] periodic 250ms fired at %ld.%06ld\n",
     //     (long)tv.tv_sec-tv_start_, (long)tv.tv_usec-tv_start_u);
-    long ttt = ((long)tv.tv_sec-tv_start_)*1000 + ((long)tv.tv_usec-tv_start_u)/1000;
-    printf("[B] periodic 250ms fired at  %ld ms\n", ttt);
+    long ttt = ((long)tv.tv_sec-tv_start_)*1000000 + ((long)tv.tv_usec-tv_start_u);
+    float ttt2 = (float)ttt/1000;
+    printf("[B] periodic 250ms fired at  %f ms\n", ttt2);
 }
 void cb_C(void *arg){
     struct timeval tv;
@@ -37,7 +39,7 @@ void cb_C(void *arg){
     // printf("[C] periodic 700ms fired at %ld.%06ld\n",
     //        (long)tv.tv_sec, (long)tv.tv_usec);
 
-    long ttt = ((long)tv.tv_sec-tv_start_)*1000 + ((long)tv.tv_usec-tv_start_u)/1000;
-    printf("[C] periodic 700ms fired at %ld ms\n", ttt);
-
+    long ttt = ((long)tv.tv_sec-tv_start_)*1000000 + ((long)tv.tv_usec-tv_start_u);
+    float ttt2 = (float)ttt/1000;
+    printf("[C] periodic 700ms fired at  %f ms\n", ttt2);
 }
